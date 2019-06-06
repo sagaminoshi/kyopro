5ff8a06b15e95812b3d2bcf6311f5043f40f3dd1
sagamino <47517465+sagaminoshi@users.noreply.github.com>
2019-06-07 00:05:25 +0900

    Update Sieve_of_Eratosthenes.py

--- Sieve_of_Eratosthenes.py
+++ Sieve_of_Eratosthenes.py
@@ -2,6 +2,7 @@
 
 import math
 def Sieve_of_Eratosthenes(N):
+    if N<2: return set()
     prime=set(i for i in range(2,N+1))
     for i in range(2,math.ceil(N**0.5)+1):
         tmp=set()
@@ -10,4 +11,21 @@ def Sieve_of_Eratosthenes(N):
         for j in tmp:
             prime.remove(j)
 
-    return(prime)
+    return prime
+
+#リスト版、こっちの方が高速。
+def sieve(N):
+    if N<2:
+        prime = [False for i in range(N+1)]
+        return prime
+ 
+    prime = [True for i in range(N+1)]
+    prime[0] = False
+    prime[1] = False
+ 
+    for i in range(2, int(N**0.5)+1):
+        if prime[i]:
+            for j in range(i*2, N+1, i):
+                prime[j] = False
+ 
+    return prime
